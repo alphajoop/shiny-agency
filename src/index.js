f8e8ccdd15a56806e9dc6b4c1b81328bc0ab5a4b
Alpha DIOP <alphakhoss@gmail.com>
2023-10-02 11:02:18 +0000

    init: data sharing

--- src/index.js
+++ src/index.js
@@ -8,31 +8,28 @@ import Header from './components/Header';
 import Error from './components/Error';
 import Results from './pages/Results';
 import Freelances from './pages/Freelances';
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    font-family: 'Trebuchet MS', Helvetica, sans-serif;
-  }
-
-  body {
-    margin: 0;
-  }
-`;
+import { SurveyProvider, ThemeProvider } from './utils/context';
+import Footer from './components/Footer';
+import GlobalStyle from './utils/style/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/survey/:questionNumber" element={<Survey />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/freelances" element={<Freelances />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/survey/:questionNumber" element={<Survey />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/freelances" element={<Freelances />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
 );
